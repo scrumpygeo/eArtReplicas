41dc98a4730141073acca9483c4613cec0818fe9
scrumpygeo <scrumpygeo@yahoo.co.uk>
2023-06-21 13:54:29 +0100

    add react-bootstrap, header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to eArtReplicas</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 export default App
