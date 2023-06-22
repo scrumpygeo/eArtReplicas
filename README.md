bcaff529e2d9f0c235e375d85e23f3bffe70bff6
scrumpygeo <scrumpygeo@yahoo.co.uk>
2023-06-22 09:04:12 +0100

    add react-router

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eArtReplicas
+
 2023 (dummy) version of http://www.e-artreplicas.com/
+
+Uses react-bootstrap.
+To reduce clutter due to there being many routes, these were put in index.js instead of App.js as most do.
+(NB. As we're using react-bootstrap in Header component, we need react-router-bootstrap to use LinkContainer with bootstrap elements (the equivalent of Link substituting href ))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to eArtReplicas</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
