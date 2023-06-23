c9134f7e35d18e5caf7c76929cf4f1b291c29635
scrumpygeo <scrumpygeo@yahoo.co.uk>
2023-06-23 09:04:40 +0100

    add nodemon and concurrently for dev

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import products from './data/products.js'
 
 const port = 5000
 
@@ -6,6 +7,15 @@ const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running 👍')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(p => p._id === req.params.id)
+  res.json(product)
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port} 😁`))
